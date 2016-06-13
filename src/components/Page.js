723d81f96023534b8d61db2f0d2edb190d41b8f7
dendavidov <NKbKc4GM>
2016-06-14 01:39:54 +0300

    Создание actions

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,9 +1,20 @@
 import React, { PropTypes, Component } from 'react';
 
 export default class Page extends Component {
+
+    onYearBtnClick(e) {
+        this.props.setYear(+e.target.textContent)
+    }
+
     render() {
         const { year, photos } = this.props;
         return <div>
+            <p>
+                <button onClick={::this.onYearBtnClick}>2016</button>
+                <button onClick={::this.onYearBtnClick}>2015</button>
+                <button onClick={::this.onYearBtnClick}>2014</button>
+            </p>
+            <h3>{ year} year</h3>
             <p>You have {photos.length} photos for {year} year!</p>
         </div>
     }
@@ -11,5 +22,6 @@ export default class Page extends Component {
 
 Page.propTypes = {
     year: PropTypes.number.isRequired,
-    photos: PropTypes.array.isRequired
+    photos: PropTypes.array.isRequired,
+    setYear: PropTypes.func.isRequired
 }
