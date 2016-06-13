33d4cf10c00f332f18c4bb4828216b10879b74a7
dendavidov <NKbKc4GM>
2016-06-14 01:58:54 +0300

    Наводим порядок

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './containers/App'
-import configureStore from './store/configureStore'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import './styles/app.css';
+import configureStore from './store/configureStore';
 
-const store = configureStore()
+const store = configureStore();
 
 render(
     <Provider store={store}>
-        <App />
+        <div className='app'>
+            <App />
+        </div>
     </Provider>,
     document.getElementById('root')
-)
+);
